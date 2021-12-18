560629720c5a96c9222293c43d0f748573c7a5d7
Rupesh Agrawal <rupeshagrawal008@gmail.com>
2021-12-18 16:01:00 +0530

    second commit

--- gs3/gs3/settings.py
+++ gs3/gs3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i!90t0xawi(g+uq4_+(h3q5q1-s+^lfg&il-2^$%)izq!2f0nr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1:8000/admin/api/student/]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- gs3/gs3/urls.py
+++ gs3/gs3/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from api import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('studentapi/', views.student_api),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
