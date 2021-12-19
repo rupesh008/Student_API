2b55b8b89a2c4752e96175ffadbb1b8452bb946e
Rupesh Agrawal <rupeshagrawal008@gmail.com>
2021-12-19 21:20:18 +0530

    initial commi

--- gs3/gs3/settings.py
+++ gs3/gs3/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i!90t0xawi(g+uq4_+(h3q5q1-s+^lfg&il-2^$%)izq!2f0nr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','studentapiapp.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','studentapiapp.herokuapp.com']
 
 
 # Application definition
