7dc8104b2323d9115a782efe354a970bdcc1b8ce
Rupesh Agrawal <rupeshagrawal008@gmail.com>
2021-12-21 13:38:58 +0530

    seven commit

--- gs3/gs3/urls.py
+++ gs3/gs3/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from api import views
 
 from django.views.static import serve
-from django.conf.urls import url
+#from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
